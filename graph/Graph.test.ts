c4248d09956f908cbb8c17ac6a0389cb444cf2f4
Artur Luis <43670562+ArturLuisOliveira@users.noreply.github.com>
2023-08-10 08:37:50 -0300

    redo graph class

--- graph/Graph.test.ts
+++ graph/Graph.test.ts
@@ -1,278 +1,38 @@
 import { Graph } from "./Graph";
 
 describe("graph", () => {
-  describe("#addVertex", () => {
-    test("should add the given vertices", () => {
-      const graph = new Graph<string>();
-      graph.addVertex("A");
-      graph.addVertex("B");
-      graph.addVertex("C");
-      graph.addVertex("D");
-      expect(graph.vertices).toEqual(["A", "B", "C", "D"]);
-    });
-    test("should not add repeated vertices", () => {
-      const graph = new Graph<string>();
-      graph.addVertex("A");
-      graph.addVertex("A");
-      expect(graph.vertices).toEqual(["A"]);
-    });
-  });
   describe("#addEdge", () => {
-    describe("when the graph is directed", () => {
-      test("should add edges to the given new vertices", () => {
-        //https://media.geeksforgeeks.org/wp-content/uploads/20201208095819/UntitledDiagram9.jpg
-        const graphA = new Graph<string>({ isDirected: true });
-        graphA.addEdge({ vertexA: "0", vertexB: "1" });
-        graphA.addEdge({ vertexA: "1", vertexB: "2" });
-        graphA.addEdge({ vertexA: "2", vertexB: "0" });
-        expect(graphA.vertices.sort()).toEqual(["0", "1", "2"].sort());
-        expect(graphA.adjacencyList.get("0")).toEqual(["1"]);
-        expect(graphA.adjacencyList.get("1")).toEqual(["2"]);
-        expect(graphA.adjacencyList.get("2")).toEqual(["0"]);
-
-        // https://media.geeksforgeeks.org/wp-content/uploads/20201208100338/UntitledDiagram10.jpg
-        const graphB = new Graph<string>({ isDirected: true });
-        graphB.addEdge({ vertexA: "0", vertexB: "1" });
-        graphB.addEdge({ vertexA: "1", vertexB: "2" });
-        graphB.addEdge({ vertexA: "2", vertexB: "3" });
-        graphB.addEdge({ vertexA: "3", vertexB: "0" });
-        graphB.addEdge({ vertexA: "1", vertexB: "3" });
-
-        expect(graphB.vertices.sort()).toEqual(["0", "1", "2", '3'].sort());
-        expect(graphB.adjacencyList.get("0")).toEqual(["1"]);
-        expect(graphB.adjacencyList.get("1")?.sort()).toEqual(["2", '3'].sort());
-        expect(graphB.adjacencyList.get("2")).toEqual(["3"]);
-        expect(graphB.adjacencyList.get("3")).toEqual(["0"]);
-
-      });
-      test("should add edges to the given existent vertices", () => {
-        const graph = new Graph<string>({ isDirected: true });
-        graph.addVertex("A");
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-
-        expect(graph.vertices.sort()).toEqual(["A", "B"].sort());
-        expect(graph.adjacencyList.get("A")).toEqual(["B"]);
-        expect(graph.adjacencyList.get("B")).toEqual([]);
-      });
-      test("should not add repeated edges", () => {
-        const graph = new Graph<string>({ isDirected: true });
-        graph.addVertex("A");
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-        expect(graph.vertices.sort()).toEqual(["A", "B"].sort());
-        expect(graph.adjacencyList.get("A")).toEqual(["B"]);
-        expect(graph.adjacencyList.get("B")).toEqual([]);
-      });
-    });
-    describe("when the graph isn't directed", () => {
-      test("should add edges to the given new vertices", () => {
-        const graph = new Graph<string>();
-
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-
-        expect(graph.vertices).toEqual(["A", "B"]);
-        expect(graph.adjacencyList.get("A")).toEqual(["B"]);
-        expect(graph.adjacencyList.get("B")).toEqual(["A"]);
-      });
-      test("should add edges to the given existent vertices", () => {
-        const graph = new Graph<string>();
-        graph.addVertex("A");
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-
-        expect(graph.vertices).toEqual(["A", "B"]);
-        expect(graph.adjacencyList.get("A")).toEqual(["B"]);
-        expect(graph.adjacencyList.get("B")).toEqual(["A"]);
-      });
-      test("should not add repeated edges", () => {
-        const graph = new Graph<string>();
-        graph.addVertex("A");
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-        expect(graph.vertices).toEqual(["A", "B"]);
-        expect(graph.adjacencyList.get("A")).toEqual(["B"]);
-        expect(graph.adjacencyList.get("B")).toEqual(["A"]);
-      });
-    });
-  });
-  describe("#toString", () => {
-    test("should print directed graph", () => {
-      //https://w7.pngwing.com/pngs/233/295/png-transparent-directed-graph-directed-acyclic-graph-vertex-graph-theory-data-graph-angle-white-text.png
-      const graph = new Graph<string>({ isDirected: true });
-      graph.addEdge({ vertexA: "A", vertexB: "B" });
-      graph.addEdge({ vertexA: "B", vertexB: "C" });
-      graph.addEdge({ vertexA: "B", vertexB: "D" });
-      graph.addEdge({ vertexA: "B", vertexB: "E" });
-      graph.addEdge({ vertexA: "G", vertexB: "D" });
-      graph.addEdge({ vertexA: "C", vertexB: "E" });
-      graph.addEdge({ vertexA: "D", vertexB: "E" });
-      graph.addEdge({ vertexA: "E", vertexB: "F" });
-
-
-      expect(graph.toString()).toBe("A -> B\nB -> C D E\nC -> E\nD -> E\nE -> F\nF ->\nG -> D");
-    });
-    test("should print non directed graph", () => {
-      //https://progressivecoder.com/wp-content/uploads/2020/12/graph_example.png
+    describe("given a graph", () => {
       const graph = new Graph<string>();
-      graph.addEdge({ vertexA: "0", vertexB: "1" });
-      graph.addEdge({ vertexA: "0", vertexB: "4" });
-      graph.addEdge({ vertexA: "1", vertexB: "4" });
-      graph.addEdge({ vertexA: "1", vertexB: "3" });
-      graph.addEdge({ vertexA: "1", vertexB: "2" });
-      graph.addEdge({ vertexA: "2", vertexB: "3" });
-      graph.addEdge({ vertexA: "4", vertexB: "3" });
 
-      expect(graph.toString()).toBe("0 -> 1 4\n1 -> 0 2 3 4\n2 -> 1 3\n3 -> 1 2 4\n4 -> 0 1 3");
+      it("should add the edges", () => {
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "C");
+        graph.addEdge("B", "D");
+
+        expect(graph.adjList.get("A")).toEqual(["B"]);
+        expect(graph.adjList.get("B")).toEqual(["C", "D"]);
+        expect(graph.adjList.get("C")).toEqual([]);
+        expect(graph.adjList.get("D")).toEqual([]);
+      });
     });
   });
-  describe('#breadthFirstSearch', () => {
-    describe('given a directed graph', () => {
-      //https://webeduclick.com/wp-content/uploads/2021/02/Breadth-First-Search-in-Artificial-Intelligence.jpg
-      const graph = new Graph<string>({ isDirected: false });
-      graph.addEdge({ vertexA: "ROOT", vertexB: "A" });
-      graph.addEdge({ vertexA: "ROOT", vertexB: "B" });
-      graph.addEdge({ vertexA: "ROOT", vertexB: "C" });
-      graph.addEdge({ vertexA: "A", vertexB: "D" });
-      graph.addEdge({ vertexA: "A", vertexB: "E" });
-      graph.addEdge({ vertexA: "B", vertexB: "F" });
-      graph.addEdge({ vertexA: "C", vertexB: "G" });
-      test('should run the breadth first search in the given order', () => {
-        const breadthFirstSearch: string[] = []
-        graph.breadthFirstSearch({ callback: (vertex) => breadthFirstSearch.push(vertex), vertex: 'ROOT' })
 
-        expect(breadthFirstSearch[0]).toBe("ROOT");
-        ["A", "B", "C"].forEach(vertex => {
-          expect(breadthFirstSearch.indexOf(vertex)).toBeGreaterThanOrEqual(1)
-          expect(breadthFirstSearch.indexOf(vertex)).toBeLessThanOrEqual(3)
-        });
-        ["D", "E", "F", "G"].forEach(vertex => {
-          expect(breadthFirstSearch.indexOf(vertex)).toBeGreaterThanOrEqual(4)
-          expect(breadthFirstSearch.indexOf(vertex)).toBeLessThanOrEqual(7)
-        })
+  describe("#addVertex", () => {
+    describe("given a graph", () => {
+      const graph = new Graph<string>();
 
-      })
-    })
-    describe('given a non directed graph', () => {
-      //https://webeduclick.com/wp-content/uploads/2021/02/Breadth-First-Search-in-Artificial-Intelligence.jpg
-      const graph = new Graph<string>({ isDirected: true });
-      graph.addEdge({ vertexA: "ROOT", vertexB: "A" });
-      graph.addEdge({ vertexA: "ROOT", vertexB: "B" });
-      graph.addEdge({ vertexA: "ROOT", vertexB: "C" });
-      graph.addEdge({ vertexA: "A", vertexB: "D" });
-      graph.addEdge({ vertexA: "A", vertexB: "E" });
-      graph.addEdge({ vertexA: "B", vertexB: "F" });
-      graph.addEdge({ vertexA: "C", vertexB: "G" });
-      test('should run the breadth first search in the given order', () => {
-        const breadthFirstSearch: string[] = []
-        graph.breadthFirstSearch({ callback: (vertex) => breadthFirstSearch.push(vertex), vertex: 'ROOT' })
+      it("should add a new vertex", () => {
+        graph.addVertex("A");
+        graph.addVertex("B");
 
-        expect(breadthFirstSearch[0]).toBe("ROOT");
-        ["A", "B", "C"].forEach(vertex => {
-          expect(breadthFirstSearch.indexOf(vertex)).toBeGreaterThanOrEqual(1)
-          expect(breadthFirstSearch.indexOf(vertex)).toBeLessThanOrEqual(3)
-        });
-        ["D", "E", "F", "G"].forEach(vertex => {
-          expect(breadthFirstSearch.indexOf(vertex)).toBeGreaterThanOrEqual(4)
-          expect(breadthFirstSearch.indexOf(vertex)).toBeLessThanOrEqual(7)
-        })
-
-      })
-    })
-  })
-  describe('#shortestPath', () => {
-    describe('given a directed graph', () => {
-      const graph = new Graph<string>({ isDirected: true });
-      graph.addEdge({ vertexA: "A", vertexB: "B" });
-      graph.addEdge({ vertexA: "A", vertexB: "C" });
-      graph.addEdge({ vertexA: "A", vertexB: "D" });
-      graph.addEdge({ vertexA: "B", vertexB: "E" });
-      graph.addEdge({ vertexA: "C", vertexB: "F" });
-      graph.addEdge({ vertexA: "D", vertexB: "G" });
-      graph.addEdge({ vertexA: "G", vertexB: "H" });
-      graph.addEdge({ vertexA: "F", vertexB: "E" });
-      graph.addEdge({ vertexA: "H", vertexB: "E" });
-      test('should return the shortest path', () => {
-        expect(graph.shortestPath("A", "E")).toEqual(["A", "B", "E"])
-      })
-    })
-    describe('given a non directed graph', () => {
-      const graph = new Graph<string>({ isDirected: false });
-      graph.addEdge({ vertexA: "A", vertexB: "B" });
-      graph.addEdge({ vertexA: "A", vertexB: "C" });
-      graph.addEdge({ vertexA: "A", vertexB: "D" });
-      graph.addEdge({ vertexA: "B", vertexB: "E" });
-      graph.addEdge({ vertexA: "C", vertexB: "F" });
-      graph.addEdge({ vertexA: "D", vertexB: "G" });
-      graph.addEdge({ vertexA: "G", vertexB: "H" });
-      graph.addEdge({ vertexA: "F", vertexB: "E" });
-      graph.addEdge({ vertexA: "H", vertexB: "E" });
-      test('should return the shortest path', () => {
-        expect(graph.shortestPath("A", "E")).toEqual(["A", "B", "E"])
-      })
-    })
-  })
-  describe('#depthFirstSearch', () => {
-    describe('given a directed graph', () => {
-      const graph = new Graph<string>({ isDirected: true });
-
-      graph.addEdge({ vertexA: "A", vertexB: "B" });
-      graph.addEdge({ vertexA: "A", vertexB: "C" });
-      graph.addEdge({ vertexA: "A", vertexB: "D" });
-      graph.addEdge({ vertexA: "B", vertexB: "E" });
-      graph.addEdge({ vertexA: "B", vertexB: "F" });
-      graph.addEdge({ vertexA: "C", vertexB: "G" });
-      graph.addEdge({ vertexA: "D", vertexB: "G" });
-      graph.addEdge({ vertexA: "D", vertexB: "H" });
-      graph.addEdge({ vertexA: "E", vertexB: "I" });
-      test('should return a depth first search', () => {
-        const values: string[] = []
-        graph.depthFirstSearch({ callback: (vertex) => values.push(vertex) })
-        expect(true).toBe(true)
-      })
-    })
-    describe('given a non directed graph', () => {
-      test('should return a depth first search', () => {
-        const graph = new Graph<string>({ isDirected: false });
-
-        graph.addEdge({ vertexA: "A", vertexB: "B" });
-        graph.addEdge({ vertexA: "A", vertexB: "C" });
-        graph.addEdge({ vertexA: "A", vertexB: "D" });
-        graph.addEdge({ vertexA: "B", vertexB: "E" });
-        graph.addEdge({ vertexA: "B", vertexB: "F" });
-        graph.addEdge({ vertexA: "C", vertexB: "G" });
-        graph.addEdge({ vertexA: "D", vertexB: "G" });
-        graph.addEdge({ vertexA: "D", vertexB: "H" });
-        graph.addEdge({ vertexA: "E", vertexB: "I" });
-        const values: string[] = []
-        graph.depthFirstSearch({ callback: (vertex) => values.push(vertex) })
-        expect(true).toBe(true)
-      })
-    })
-  })
-  describe('#topologicalOrder', () => {
-    test('should return the topological order', () => {
-      //https://www.gatevidyalay.com/wp-content/uploads/2018/07/Topological-Sort-Example.png
-      const graph = new Graph<string>({ isDirected: true });
-      graph.addEdge({ vertexA: "1", vertexB: "2" });
-      graph.addEdge({ vertexA: "1", vertexB: "3" });
-      graph.addEdge({ vertexA: "2", vertexB: "4" });
-      graph.addEdge({ vertexA: "2", vertexB: "5" });
-      graph.addEdge({ vertexA: "3", vertexB: "4" });
-      graph.addEdge({ vertexA: "3", vertexB: "6" });
-      graph.addEdge({ vertexA: "4", vertexB: "5" });
-      graph.addEdge({ vertexA: "4", vertexB: "6" });
-
-      const topologicalOrder = graph.topologicalOrder()
-      expect(topologicalOrder[0]).toBe("1");
-      ["2", "3"].forEach(vertex => {
-        expect(topologicalOrder.indexOf(vertex)).toBeGreaterThanOrEqual(1)
-        expect(topologicalOrder.indexOf(vertex)).toBeLessThanOrEqual(2)
-      })
-      expect(topologicalOrder[3]).toBe("4");
-      ["5", "6"].forEach(vertex => {
-        expect(topologicalOrder.indexOf(vertex)).toBeGreaterThanOrEqual(4)
-        expect(topologicalOrder.indexOf(vertex)).toBeLessThanOrEqual(5)
-      })
-    })
-
-  })
+        expect(graph.vertices).toEqual(["A", "B"]);
+      });
+    });
+  });
 });
--- graph/Graph.ts
+++ graph/Graph.ts
@@ -1,147 +1,34 @@
-export class Graph<Vertex> {
-  private _isDirected: boolean;
-  private _vertices: Vertex[];
-  private _adjacencyList: Map<Vertex, Vertex[]>;
+export class Graph<T> {
+  isDirected: boolean;
 
-  get vertices() {
-    return this._vertices;
+  vertices: T[] = [];
+  equalCallback: (a: T, b: T) => boolean = (a, b) => a == b;
+  adjList: Map<T, T[]> = new Map();
+
+  constructor(isDirected = false) {
+    this.isDirected = isDirected;
   }
 
-  get adjacencyList() {
-    return this._adjacencyList;
+  addEdge(from: T, to: T) {
+    this.adjList.get(from)?.push(to);
+    if (this.isDirected) this.adjList.get(to)?.push(from);
   }
 
-  constructor({ isDirected = false }: { isDirected?: boolean } = {}) {
-    this._isDirected = isDirected;
-    this._vertices = [];
-    this._adjacencyList = new Map();
-  }
-
-  addVertex(vertex: any) {
-    if (!this._vertices.includes(vertex)) {
-      this._vertices.push(vertex);
-      this._adjacencyList.set(vertex, []);
+  addVertex(vertex: T) {
+    for (const currentVertex of this.vertices) {
+      if (this.equalCallback(vertex, currentVertex)) return;
     }
-  }
-
-  addEdge({ vertexA, vertexB }: Record<"vertexA" | "vertexB", Vertex>) {
-    if (!this._vertices.includes(vertexA)) {
-      this.addVertex(vertexA);
-    }
-    if (!this._vertices.includes(vertexB)) {
-      this.addVertex(vertexB);
-    }
-    if (!this._adjacencyList.get(vertexA)?.includes(vertexB))
-      this._adjacencyList.get(vertexA)?.push(vertexB);
-    if (!this._isDirected) {
-      if (!this._adjacencyList.get(vertexB)?.includes(vertexA))
-        this._adjacencyList.get(vertexB)?.push(vertexA);
-    }
-  }
-
-  breadthFirstSearch({ callback, vertex }: { vertex: Vertex, callback: (vertex: Vertex) => void }) {
-    enum Status { Explored, Visited }
-    const statuses = new Map<Vertex, Status>()
-    const queue: Vertex[] = [vertex]
-    while (queue.length) {
-      const vertex = queue.shift()
-      if (vertex && statuses.get(vertex) !== Status.Explored) {
-        const vertices = this._adjacencyList.get(vertex)
-        vertices?.forEach(vertex => {
-          if (statuses.get(vertex) !== Status.Explored && statuses.get(vertex) !== Status.Visited) {
-            queue.push(vertex)
-            statuses.set(vertex, Status.Visited)
-          }
-        })
-        statuses.set(vertex, Status.Explored)
-        callback(vertex)
-      }
-    }
-  }
-
-  shortestPath(a: Vertex, b: Vertex) {
-    const pathAndCostMap = new Map<Vertex, { path?: Vertex, cost: number }>([[a, { cost: 0 }]])
-    let previous = a
-    this.breadthFirstSearch({
-      callback: (vertex) => {
-        this._adjacencyList.get(vertex)?.forEach(adjencent => {
-          if (!pathAndCostMap.get(adjencent)) pathAndCostMap.set(adjencent, { cost: (pathAndCostMap.get(vertex)?.cost ?? 0) + 1, path: vertex })
-        })
-      },
-      vertex: a
-    })
-    const path = []
-    let next: Vertex | undefined = b
-    while (next) {
-      path.push(next)
-      next = pathAndCostMap.get(next)?.path
-    }
-    return path.reverse()
-  }
-
-  depthFirstSearch({ callback }: { callback?: (vertex: Vertex) => void } = {}) {
-    enum Color { White, Gray, Black }
-    const color = new Map<Vertex, Color>()
-    const discovery = new Map<Vertex, number>()
-    const finished = new Map<Vertex, number>()
-    const predecessor = new Map<Vertex, Vertex | null>()
-    const time = { count: 0 }
-
-    this.vertices.forEach(vertex => {
-      color.set(vertex, Color.White)
-      discovery.set(vertex, 0)
-      finished.set(vertex, 0)
-      predecessor.set(vertex, null)
-    })
-
-    const visit = (vertex: Vertex) => {
-      color.set(vertex, Color.Gray)
-      discovery.set(vertex, ++time.count)
-      const neighbours = this.adjacencyList.get(vertex)
-      neighbours?.forEach(neighbour => {
-        if (color.get(neighbour) === Color.White) {
-          predecessor.set(neighbour, vertex)
-          visit(neighbour)
-        }
-      })
-      callback && callback(vertex)
-      color.set(vertex, Color.Black)
-      finished.set(vertex, ++time.count)
-    }
-
-    this.vertices.forEach(vertex => {
-      if (color.get(vertex) === Color.White) visit(vertex)
-    })
-
-    return { discovery, finished, predecessor }
-  }
-
-  /**
-   * - Lists all vertises in a order which all the vertex who depends on another will be displayed after the vertex it depends on.
-   * - Needs to be an DAG(Directed Acyclic Graph) graph.
-   */
-  topologicalOrder(): Vertex[] {
-    const { finished } = this.depthFirstSearch()
-    const finishedArray: [Vertex, number][] = []
-    finished.forEach((value, key) => finishedArray.push([key, value]))
-
-    return finishedArray.sort((a, b) => {
-      if (a[1] > b[1]) return 1
-      if (a[1] < b[1]) return -1
-      return 0
-    }).map(([key, value]) => key).reverse()
+    this.vertices.push(vertex);
+    this.adjList.set(vertex, []);
   }
 
   toString() {
-    let string: String = "";
-    this.vertices.sort().forEach((vertex) => {
-      string = string.concat(
-        `${vertex} ->${this.adjacencyList.get(vertex)?.sort().reduce(
-          (acc, cur, index, values) => `${acc} ${cur}`,
-          ""
-        )}\n`
-      );
+    let string = "";
+
+    this.adjList.forEach((value, key) => {
+      string = `${string}\n${String(key)}: ${String(value)}`;
     });
-    return string.substring(0, string.length - 1);
+
+    return string;
   }
 }
